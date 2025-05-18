7bbc6934998956c0fdf46cf96d63ce4e121cd6e3
Adham Hussameldin Ramadan <adhamhussameddin90@gmail.com>
2025-05-18 22:35:44 +0300

    initial.

---
+++ Algorithms.cs
@@ -0,0 +1,38 @@
+﻿using Accord.Audio.Windows;
+using Recorder.MFCC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+
+namespace Recorder
+{
+    class Algorithms
+    {
+        public double dynamicTimeWarping(Sequence A, Sequence B) // Ibrahim & Zamel
+        {
+            return -1.0;
+        }
+
+        public double dynamicTimeWarpingWithPruning(Sequence A, Sequence B) // Ebrahim & Adham
+        {
+
+            return -1.0;
+        }
+
+        public void enroll() // Ebrahim & Adham
+        {
+
+        }
+
+        public string identify(Sequence A) // Ibrahim & Zamel
+        {
+
+            return "LOL";
+        }
+
+    }
+}
