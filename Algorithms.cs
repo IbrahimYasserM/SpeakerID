e98301b69546fa6a4b3a563efd9eadf777503a7d
Omar Zamel <45265324+0xZamel@users.noreply.github.com>
2025-05-19 03:27:50 +0300

    Update Algorithms.cs

--- Algorithms.cs
+++ Algorithms.cs
@@ -11,8 +11,28 @@ namespace Recorder
 {
     class Algorithms
     {
+        // get the elidean distance between two frames
+        // each frame has 13 coefficients (features).
+        public double getuEuclideanDistance(double[] frameA, double[] frameB)
+        {
+            double res = 0;
+            for (int i = 0; i < 13; i++)
+            {
+                res += (frameA[i] - frameB[i]) * (frameA[i] - frameB[i]);
+            }
+            res = Math.Sqrt(res);
+            return res;
+        }
+
         public double dynamicTimeWarping(Sequence A, Sequence B) // Ibrahim & Zamel
         {
+            // Each sequence is a list of frames.
+            // we want to compute min distance between the two sequences USING DP.
+            //Transtions:
+            // 1. Next input frame aligns to same template frame (stretching).
+            // 2. Next input frame aligns to next template frame (no warping).
+            // 3. Next input frame skips one template frame (shrinking, at most 1/2).
+
             return -1.0;
         }
 
